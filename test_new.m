97043c097acae0442527ee95b81e0e3e536fc2d6
xiaochen1111 <2972114057@qq.com>
2019-09-12 16:04:38 +0800

    Signed-off-by: xiaochen1111 <2972114057@qq.com>

--- test_new.m
+++ test_new.m
@@ -15,40 +15,51 @@ y1=boost1(j,2);
 y2=boost1(k,3);
 y3=boost1(m,4);
 y4=boost1(n,5);
+%% 初始化
 pca=[];
 cspca=[];
 % for w=30:5:65
-w=45;               %信噪比
-r=0.5;
+w=30;               %信噪比
+r=0.5;              %压缩率
 pca=[];
 pca1=[];
 pca2=[];
 pca3=[];
-%%
-% for r=0.2:0.1:0.8
-% for K=2:2:169 
-K=108;
-sig=compressed(y,w,r,K);
-sig1=compressed(y1,w,r,K);
-sig2=compressed(y2,w,r,K);
-sig3=compressed(y3,w,r,K);
-sig4=compressed(y4,w,r,K);
-% sig5=compressed(y5,w,r,K);
-%% 特征提取
+% 添加噪声
 yw=awgn(y, w,'measured', 9);
 yw1=awgn(y1,w,'measured', 9);
 yw2=awgn(y2,w,'measured',9);
 yw3=awgn(y3,w,'measured', 9);
 yw4=awgn(y4,w,'measured',9);
-% yw5=y5+w;
-% PCA
-[pcaout]=feaext(yw);
-[pcaout1]=feaext(yw1);
-[pcaout2]=feaext(yw2);
-[pcaout3]=feaext(yw3);
-[pcaout4]=feaext(yw4);
-% [pcaout4]=feaext(yw1);
-% [pcaout5]=feaext(yw2);
+
+%% 重构
+% for r=0.2:0.1:0.8
+% for K=2:2:169 
+K=112;
+sig=compressed(y,w,r,K);
+sig1=compressed(y1,w,r,K);
+sig2=compressed(y2,w,r,K);
+sig3=compressed(y3,w,r,K);
+sig4=compressed(y4,w,r,K);
+
+%% 时域特征提取 退化指标的建立
+[me,ma,mi,st,fc,pk ,rmse ,fgfz ,sf ,cf ,If ,Clf ,Kv]=time_feaext(sig);
+[me1,ma1,mi1,st1,fc1,pk1,rmse1,fgfz1,sf1,cf1,If1,Clf1,Kv1]=time_feaext(sig1);
+[me2,ma2,mi2,st2,fc2,pk2,rmse2,fgfz2,sf2,cf2,If2,Clf2,Kv2]=time_feaext(sig2);
+[me3,ma3,mi3,st3,fc3,pk3,rmse3,fgfz3,sf3,cf3,If3,Clf3,Kv3]=time_feaext(sig3);
+[me4,ma4,mi4,st4,fc4,pk4,rmse4,fgfz4,sf4,cf4,If4,Clf4,Kv4]=time_feaext(sig4);
+yy=[me;ma;mi;st;fc;pk;rmse;fgfz;sf;cf;If;Clf;Kv];
+yy1=[me1;ma1;mi1;st1;fc1;pk1;rmse1;fgfz1;sf1;cf1;If1;Clf1;Kv1];
+yy2=[me2;ma2;mi2;st2;fc2;pk2;rmse2;fgfz2;sf2;cf2;If2;Clf2;Kv2];
+yy3=[me3;ma3;mi3;st3;fc3;pk3;rmse3;fgfz3;sf3;cf3;If3;Clf3;Kv3];
+yy4=[me4;ma4;mi4;st4;fc4;pk4;rmse4;fgfz4;sf4;cf4;If4;Clf4;Kv4];
+%% PCA特征提取
+[pcaout]=feaext(yy);
+[pcaout1]=feaext(yy1);
+[pcaout2]=feaext(yy2);
+[pcaout3]=feaext(yy3);
+[pcaout4]=feaext(yy4);
+ 
 %% 评价指标
 D=evalution(pcaout,pcaout1);
 D1=evalution(pcaout,pcaout2);
